bc692d3a52017f07fb0bbe6d0242bce282a5a7d3
Kurt W. Pasewaldt <kurt@pasewaldt.com>
2014-02-11 10:04:04 -0500

    new pages

---
+++ QFlexvideos.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'header.html';
+
+echo <<<_END
+    <h3>Genband (S3) QUANTiX QFlex Videos</h3>
+    <ul>
+        <li><a href="http://www.youtube.com/watch?v=eG2nWM07XAk">Genband QFlex CDRs</a></li>
+        <li><a href="http://www.youtube.com/watch?v=GdoR_UAvnxo">Genband QFlex Packet Capture</a></li>
+        <li><a href="http://www.youtube.com/watch?v=I2hHp1Zah_0">Genband QFlex Prepend Digits WAN</a></li>
+        <li><a href="http://www.youtube.com/watch?v=rNZEYL_9m80">Genband QFlex OS Install</a></li>
+        <li><a href="http://www.youtube.com/watch?v=wYxDB5I9BmA">Genband QFlex Prepend LAN</a></li>
+        <li><a href="http://www.youtube.com/watch?v=hoP8O5_3fcw">Genband QFlex Call Trace</a></li>
+    </ul>
+    </div>
+    </body>
+    </html>
+_END;
+?>
---
+++ S3videos.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'header.html';
+
+echo <<<_END
+    <h3>Genband (S3) QUANTiX SBC Videos</h3>
+    <ul>
+        <li><a href="http://www.youtube.com/watch?v=hXI6jA6H234">Genband S3 Media Tracing P1</a></li>
+        <li><a href="http://www.youtube.com/watch?v=RgtaTzrUXv4">Genband S3 Media Tracing P2</a></li>
+    </ul>
+    </div>
+    </body>
+    </html>
+_END;
+?>
