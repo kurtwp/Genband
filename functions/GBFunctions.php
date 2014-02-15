246082a9d0f02f990ead67f4f5ffc59638665be1
Kurt W. Pasewaldt <kurt@pasewaldt.com>
2014-02-15 11:18:28 -0500

    added new functions

--- functions/GBFunctions.php
+++ functions/GBFunctions.php
@@ -14,20 +14,46 @@ function validateIP($ip)
 	return $ip . " is not a valid IP address ! <br />";
     }
 }
+// Validate Port Numbers and Positive Numbers only
 function validatePort($port) {
     
     // Reg Ex checks for postive numbers only
     $regexp = '/^\d+$/';
-    
-    if ($port != "") {
-        if (preg_match($regexp, $port))
-        {
+    $arraySize = count($port);
+    for ($f=0; $f<$arraySize; $f++) {
+        if ($port[$f] != "") {
+            if (preg_match($regexp, $port[$f]))
+            {
+               
+            } else {
+                return "Port number must be all numbers !";
+            }
+        } else {
+            return "Please enter a Port Number !<br />";
+        }
+    }
+    return;
+}
+// Compare Port Count with Realm Count
+function compareRSACount($PCount,$RCount) {
+    if ($PCount != $RCount) {
+        if ($RCount ==1 ) {
             return "";
         } else {
-            return "Port number must be all numbers !";
+            return "Missing Realm or Ports";
         }
-    } else {
-        return "Please enter a Port Number !<br />";
     }
+    return "";
+}
+// Compare Port Count with URI Count
+function compareURICount($PCount,$UCount) {
+    if ($PCount != $UCount) {
+        if ($UCount ==1 ) {
+            return "";
+        } else {
+            return "Missing URI or Ports";
+        }
+    }
+    return "";
 }
 ?>
