921c13a2063039d69458bc22e54ae70b1ad917fb
Kurt W. Pasewaldt <kurt@pasewaldt.com>
2014-02-15 11:19:00 -0500

    added new functions

--- js/GBvalidate.js
+++ js/GBvalidate.js
@@ -14,8 +14,59 @@
 		}
 	}
 	
+// Validate Port Numbers and Positive Numbers
+	function validatePort(field) {
+		var regexp = /^\d+$/;
+		
+		var tempArray = field.split(",");
+		var arraySize = tempArray.length;
+		for (i=0;i<arraySize;i++) {
+			if (tempArray[i] != "") {
+				if (tempArray[i].match(regexp)) {
+					
+				} else {
+					return "Port number must be all numbers! \n";
+				}
+			} else {
+				return "Please enter a Port Number(s)! \n";
+			}
+		}
+		return "";
+		
+	}
+// Compare the amout of ports to amount of RSA 
+	function compareRSAPort(var1,var2) {
+		var countRSA = var1.split(",");
+		var countPort = var2.split(",");
+		var sizeRSA = countRSA.length;
+		var sizePort = countPort.length;
+		if (sizePort != sizeRSA) {
+			if (sizeRSA == 1) {
+				return "";
+			} else {
+				return "Missing Realms or Ports";
+			}
+		} 
+		return "";
+	}
+// Compare the amout of ports to amount of URIs 
+	function compareURIPort(var1,var2) {
+		var countURI = var1.split(",");
+		var countPort = var2.split(",");
+		var sizeURI = countURI.length;
+		var sizePort = countPort.length;
+		if (sizePort != sizeURI) {
+			return "Missing URIs"
+		} else {
+			return "";
+		}
+	}
+	
 	function GBvalidate(form) {
-		fail  = validateIP(form.epIP.value)
+		fail = validatePort(form.epPort.value)
+		fail += validateIP(form.epIP.value)
+		fail += compareRSAPort(form.epRealm.value,form.epPort.value)
+		fail += compareURIPort(form.epURI.value,form.epPort.value)
 		if (fail == "") return true
 		else { alert(fail); return false }
 	}
