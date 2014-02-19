93c82b2c0717d5ded915b6217c1ae0b6d4ad8701
Kurt W. Pasewaldt <kurt@pasewaldt.com>
2014-02-19 10:50:46 -0500

    added more functions

--- js/GBvalidate.js
+++ js/GBvalidate.js
@@ -2,7 +2,9 @@
 	function validateIP(field){
 	
 		var regexp = /^((1?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(1?\d{1,2}|2[0-4]\d|25[0-5])$/;
-		
+		if (field == "NA") {
+			return "";
+		}
 		if (field != "") {
 			if (field.match(regexp)) {
 				return "";
@@ -17,20 +19,19 @@
 // Validate Port Numbers and Positive Numbers
 	function validatePort(field) {
 		var regexp = /^\d+$/;
-		
 		var tempArray = field.split(",");
 		var arraySize = tempArray.length;
-		for (i=0;i<arraySize;i++) {
-			if (tempArray[i] != "") {
-				if (tempArray[i].match(regexp)) {
-					
+			for (i=0;i<arraySize;i++) {
+				if (tempArray[i] != "") {
+					if (tempArray[i].match(regexp)) {
+						
+					} else {
+						return "Port number must be all numbers! \n";
+					}
 				} else {
-					return "Port number must be all numbers! \n";
+					return "Please enter a Port Number(s)! \n";
 				}
-			} else {
-				return "Please enter a Port Number(s)! \n";
 			}
-		}
 		return "";
 		
 	}
@@ -44,7 +45,7 @@
 			if (sizeRSA == 1) {
 				return "";
 			} else {
-				return "Missing Realms or Ports";
+				return "Missing Realms or Ports! \n";
 			}
 		} 
 		return "";
@@ -55,16 +56,30 @@
 		var countPort = var2.split(",");
 		var sizeURI = countURI.length;
 		var sizePort = countPort.length;
+		if (var1 == "NA") {
+			return "";
+		} else if (var1 == "") {
+			return "Need to add a URI name! \n";
+		}
 		if (sizePort != sizeURI) {
-			return "Missing URIs"
+			return "Missing URIs or Ports! \n"
 		} else {
 			return "";
 		}
 	}
-	
+// Validate if Realm Field is Filled In
+    function validateRSA(rsa) {
+        if (rsa != "") {
+            return "";
+        } else {
+            return "Missing Realm Name! \n";
+        }
+    }
+   
 	function GBvalidate(form) {
 		fail = validatePort(form.epPort.value)
 		fail += validateIP(form.epIP.value)
+		fail += validateRSA(form.epRealm.value)
 		fail += compareRSAPort(form.epRealm.value,form.epPort.value)
 		fail += compareURIPort(form.epURI.value,form.epPort.value)
 		if (fail == "") return true
