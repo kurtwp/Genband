9b5c0399cf1b853ff10e5e3ea03b4fc065946750
Kurt W. Pasewaldt <kurt@pasewaldt.com>
2014-02-21 08:48:37 -0500

    Added more functions

--- functions/GBFunctions.php
+++ functions/GBFunctions.php
@@ -14,6 +14,42 @@ function validateIP($ip)
 	return $ip . " is not a valid IP address ! <br />";
     }
 }
+// Validate Subnet MAsk
+function validateMask($mask) {
+    $firstOctet = 0;
+    $octet = [0,128,192,224,240,248,252,254,255];
+    
+    $tempOctet = explode(".",$mask);
+    $arrayCount = count($tempOctet);
+    if ($arrayCount < 4 || $arrayCount > 4) {
+        return "Invalid Subnet Mask! <be />";
+    }
+    if ($arrayCount == 4) {
+        for ($i = 0; $i<$arrayCount; $i++) {
+            if ($i == 0) {
+                if ($firstOctet == $tempOctet[0]) {
+                    return "First Mask Octet is Invalid! <br />";
+                }
+                $i = 1;
+            }
+            for ($k=0;$k<9;$k++) {
+                if ($tempOctet[$i] == $octet[$k]) {
+                    $value = 1;
+                    break;
+                } else {
+                    $value = 0;
+                    echo $value;
+                }
+                
+            }
+            if ($value == 0 && $k == 9) {
+                    return "Invalid Subnet Mask! <br />";
+                }
+        }
+    }
+    return "";
+}
+
 // Validate Port Numbers and Positive Numbers only
 function validatePort($port) {
     
@@ -26,7 +62,7 @@ function validatePort($port) {
             {
                
             } else {
-                return "Port number must be all numbers !";
+                return "Port number must be all numbers !<br />";
             }
         } else {
             return "Please enter a Port Number !<br />";
@@ -34,12 +70,34 @@ function validatePort($port) {
     }
     return;
 }
+// Validate Media Pool ID
+function validateMP($pool) {
+    // Reg Ex checks for postive numbers only
+    $regexp = '/^\d+$/';
+    if ($pool != "") {
+        if (preg_match($regexp, $pool)) {
+            return "";            
+        } else {
+            return "Media Pool ID must be a Positive Number! <br />";
+        }
+    } else {
+        return "Please Enter in a Media Pool Number! <br />";
+    }
+}
 // Validate Realm
     function validateRSA($rsa) {
         if ($rsa != "") {
             return "";
         } else {
-            return "Enter in Realm name";
+            return "Please Enter a Realm name! <br />";
+        }
+    }
+// Validate VNet
+    function validateVNet($vnet) {
+        if ($vnet != "") {
+            return "";
+        } else {
+            return "Please Enter a VNet name! <br />";
         }
     }
 // Compare Port Count with Realm Count
